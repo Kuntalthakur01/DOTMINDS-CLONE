3effe093b9758ac574f35833dd9d9cf67499e7cd
KUNTAL THAKUR <kuntalthakur01@gmail.com>
2022-04-04 22:15:23 +0530

    about page connect to the home page
    page connection is done but still facing some errors

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+
+
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/about_us')
+def about_us():
+    return render_template('about-us.html')
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
+    
+    
